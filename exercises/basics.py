391ee5b58b848b0e235f218782736a66b94542a8
MwiineIrwin <mwiineirwin23@gmail.com>
2026-05-12 11:07:44 +0300

    feat: implement collatz sequence and distinct number counting functions in basics.py

--- exercises/basics.py
+++ exercises/basics.py
@@ -1,24 +1,52 @@
 from typing import List
 
 
-def collatz(n: int) -> List[int]:
-    """
-    You're given a positive integer n. Write an algorithm that does the following:
-        - If n is even, the algorithm divides n by 2. This is the new value of n
-        - If n is odd, the algorithm multiplies it by 3 and adds 1. This is the new value of n.
-        - The algorithm repeats this until n == 1.
+#def collatz(n: int) -> List[int]:
+ #   """
+  #  You're given a positive integer n. Write an algorithm that does the following:
+   #     - If n is even, the algorithm divides n by 2. This is the new value of n
+    #    - If n is odd, the algorithm multiplies it by 3 and adds 1. This is the new value of n.
+     #   - The algorithm repeats this until n == 1.
 
-    Implement this algorithm in this function and return a list of all the intermediate values of n.
-    For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
-    So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
-    """
-    pass
+  #  Implement this algorithm in this function and return a list of all the intermediate values of n.
+    #For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+    #So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
+    #"""
+    #pass
+
+def collatz (n:int) -> list[int]:
+    sequence = [n]
+    while n != 1:
+        if n %2 ==0:
+            n = n//2
+        else:
+            n= n*3+1
+        sequence.append(n)
+    return sequence 
 
 
-def distinct_numbers(numbers: List[int]) -> int:
-    """
-    You are given a list of integers (the list could be empty), calculate the number of distinct/unique values in the list.
 
-    E.g if numbers = [2, 3, 2, 2, 3], then the answer is 2 since there are only 2 unique numbers: 2 and 3.
-    """
-    pass
+
+
+
+##def distinct_numbers(numbers: List[int]) -> int:
+  ##  """
+   ## You are given a list of integers (the list could be empty), calculate the number of distinct/unique values in the list.
+
+   ## E.g if numbers = [2, 3, 2, 2, 3], then the answer is 2 since there are only 2 unique numbers: 2 and 3.
+  ##  """
+   ## pass
+
+def distinct_numbers (numbers: list[int]) -> int:
+    unique = set(numbers)
+    return len(unique)
+
+
+
+# checking the result 
+result = collatz (7)
+print ("the result is :",result)
+
+#checking unique numbers 
+result = distinct_numbers ([1,1,1,2,2,4,4,3,9,6,6])
+print ("the numbers of unique values are :", result)
